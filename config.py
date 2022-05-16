d924fdee75fc481aba2b4027a8fa772df16b8235
nutandbolt <ajaykrao@gmail.com>
2022-05-16 16:15:29 +0530

    code clean up config file introduced

---
+++ config.py
@@ -0,0 +1,71 @@
+mapping = {
+    'Pull Set 700 M': {
+        'url': "Pull%20Broken_700M",
+        'fmt': "%M:%S.%f",
+        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
+        'plot': 'time',
+        'yaxis_label': "MM:SS:sss",
+        'header': 2
+    },
+    'Pull Set 400 M': {
+        'url': "Pull%20Broken",
+        'fmt': "%M:%S.%f",
+        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
+        'plot': 'time',
+        'yaxis_label': "MM:SS:sss",
+        'header': 2
+    },
+    'Endurance 500 M': {
+        'url': "Endurance",
+        'fmt': "%M:%S.%f",
+        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
+        'plot': 'time',
+        'yaxis_label': "MM:SS:sss",
+        'header': 1
+
+    },
+    'Kick Set 100 M': {
+        'url': "Kick%20100M",
+        'fmt': "%M:%S.%f",
+        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
+        'plot': 'time',
+        'yaxis_label': "MM:SS:sss",
+        'header': 2
+
+    },
+    'Time Trial 100 M': {
+        'url': "Time%20Trial",
+        'fmt': "%M:%S.%f",
+        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
+        'plot': 'time',
+        'yaxis_label': "MM:SS:sss",
+        'header': 2
+
+    },
+    'Continuous Swim': {
+        'url': "Continuous%20Swim",
+        'fmt': "%H:%M:%S",
+        'display_fmt': "{hours:02d}:{minutes:02d}:{seconds:02d}.{milli:03d}",
+        'plot': 'distance',
+        'yaxis_label': "H:MM:SS",
+        'header': 1
+    },
+    'Swim Broken 1000 M': {
+        'url': "Swim%20Broken",
+        'fmt': "%M:%S.%f",
+        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
+        'plot': 'time',
+        'yaxis_label': "MM:SS:sss",
+        'header': 1
+
+    },
+    'Sprint 50 M': {
+        'url': "Others",
+        'fmt': "%M:%S.%f",
+        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
+        'plot': 'time',
+        'yaxis_label': "MM:SS:sss",
+        'header': 1
+
+    }
+}
--- plot_metric.py
+++ plot_metric.py
@@ -9,11 +9,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import dateutil.parser as dparser
 import datetime
-from halloffame import hall_of_fame
-from trendline import fit_trendline
-import requests
-# import plotly.io as pio
-# pio.renderers.default = "browser"
+from config import mapping
 
 
 # utility build display string from nanoseconds
@@ -40,78 +36,6 @@ def strptime(s, time_fmt):
 
 
 sheet_id = '1MtzRZiacK93npe_i4AhJ5okC7RtKAo_3l2aWMdpHl7c'
-
-mapping = {
-    'Pull Set 700 M': {
-        'url': "Pull%20Broken_700M",
-        'fmt': "%M:%S.%f",
-        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
-        'plot': 'time',
-        'yaxis_label': "MM:SS:sss",
-        'header': 2
-    },
-    'Pull Set 400 M': {
-        'url': "Pull%20Broken",
-        'fmt': "%M:%S.%f",
-        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
-        'plot': 'time',
-        'yaxis_label': "MM:SS:sss",
-        'header': 2
-    },
-    'Endurance 500 M': {
-        'url': "Endurance",
-        'fmt': "%M:%S.%f",
-        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
-        'plot': 'time',
-        'yaxis_label': "MM:SS:sss",
-        'header': 1
-
-    },
-    'Kick Set 100 M': {
-        'url': "Kick%20100M",
-        'fmt': "%M:%S.%f",
-        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
-        'plot': 'time',
-        'yaxis_label': "MM:SS:sss",
-        'header': 2
-
-    },
-    'Time Trial 100 M': {
-        'url': "Time%20Trial",
-        'fmt': "%M:%S.%f",
-        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
-        'plot': 'time',
-        'yaxis_label': "MM:SS:sss",
-        'header': 2
-
-    },
-    'Continuous Swim': {
-        'url': "Continuous%20Swim",
-        'fmt': "%H:%M:%S",
-        'display_fmt': "{hours:02d}:{minutes:02d}:{seconds:02d}.{milli:03d}",
-        'plot': 'distance',
-        'yaxis_label': "H:MM:SS",
-        'header': 1
-    },
-    'Swim Broken 1000 M': {
-        'url': "Swim%20Broken",
-        'fmt': "%M:%S.%f",
-        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
-        'plot': 'time',
-        'yaxis_label': "MM:SS:sss",
-        'header': 1
-
-    },
-    'Sprint 50 M': {
-        'url': "Others",
-        'fmt': "%M:%S.%f",
-        'display_fmt': "{minutes:02d}:{seconds:02d}.{milli:03d}",
-        'plot': 'time',
-        'yaxis_label': "MM:SS:sss",
-        'header': 1
-
-    }
-}
 
 
 def plot_metric():
